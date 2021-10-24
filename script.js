d51a602b0f38770697061d9fbe14fa4d4ef029b9
Sergey <webcoderprof@gmail.com>
2021-10-25 01:48:50 +0300

    october commit

---
+++ script.js
@@ -0,0 +1,47 @@
+ " use strict ";
+
+let number = 5; 
+const BorderLeftBottom = 1;
+
+number = 10;
+console.log (number);
+
+const obj = {
+    name: 'Sergey',
+    age: 23,
+    isMarried: false
+};
+
+console.log(obj.age);
+
+let firstArr = ['apple', 'juise', 23, false, {}, [], NaN];
+console.log(firstArr[3]);
+
+//const age = +prompt("Вам есть 18?", "18");
+//console.log(typeof(age));
+
+//const answers = [];
+
+//answers [0] = prompt("Как вас зовут?", "");
+//answers [1] = prompt("Как ваша фамилия?", "");
+//answers [2] = prompt("Сколько вам лет?", "");
+
+//document.write (answers);
+
+
+//const user = "Sergey";
+
+//alert(`Привет, ${user}`);
+
+//ИНТЕРПОЛЯЦИЯ
+
+const isChecked = true,
+      isClose = true;
+      
+console.log(isChecked && !isClose);
+
+console.log(isChecked || !isClose);
+
+//ОПЕРАТОРЫ В JS
+
+
