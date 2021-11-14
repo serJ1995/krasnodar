55cc9511bf1da95e19da2420a6ea99d758959a0f
Sergey <webcoderprof@gmail.com>
2021-11-14 16:53:02 +0300

    cycles

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
 
 ' use strict';
 
-if ( 4==9){
-    console.log('Ok!');
-} else{
-    console.log('Error');
+let num = 50;
+
+/*while (num < 55){
+    console.log(num);
+    num++;
 }
 
-
-/*const num = 50;
-
-if (num < 49){
-    console.log('Error');
-} else if(num > 100){
-    console.log('Много');
-} else{
-    console.log('Ok');
+do {
+    console.log(num);
+    num++;
 }
+while (num < 55);*/
 
-(num === 50) ? console.log('Ok!') : console.log('Error');*/
-
-const num = 50;
-
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('В точку!');
-        break;    
-    default:
-        console.log('Не в этот раз');
-        break;    
+for (let i = 1; i < 10; i++) {
+    if( i === 6){
+    //break;
+    continue;
+}
+    console.log(i);
 }
