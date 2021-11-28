708bb087497c2c49f97fbf0bed4e37b2701d4cee
Sergey <webcoderprof@gmail.com>
2021-11-29 01:26:29 +0300

    destruct_of_obj

--- script.js
+++ script.js
@@ -8,7 +8,34 @@ const options = {
     colors: {
         border: 'black',
         bg: 'red'
+    },
+     makeTest: function() {
+        console.log('Tset');
     }
 };
 
-console.log(options.name);
+options.makeTest();
+
+const {border, bg} = options.colors; //ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА
+console.log(border);
+
+//console.log(Object.keys(options).length); //ОПРЕДЕЛЯЕМ КОЛИЧЕСТВО КЛЮЧЕЙ В ОБЪЕКТЕ
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+
+
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    }else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+    
+}
