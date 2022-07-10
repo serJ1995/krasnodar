0f1411a3cbf22bed380dc1e9e29293d9ad4739e7
Serj <webcoderprof@gmail.com>
2022-07-10 14:31:37 +0300

    practice_width_objects

--- script.js
+++ script.js
@@ -1,112 +1,126 @@
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+
 ' use strict ';
 
-let a = 5,
-    b = a;
-b = b + 5;
-console.log(b);
-console.log(a);
-
-// const obj = {
-//     a : 5,
-//     b : 1
-// };
-
-// const copy = obj; //ссылка на уже существующий объект
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
 
 
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for ( key in mainObj) {
-        objCopy[key] = mainObj[key];
+const personalMovieDB = {
+   count: 0,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+   start: function() {
+    personalMovieDB.count = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+        personalMovieDB.count = +prompt ('Сколько фильмов вы уже посмотрели?', '');
     }
+},
+    rememberMyFilms: function() {
+    for ( let i = 0; i < 2; i++ ) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+  
+              if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+                  personalMovieDB.movies[a] = b;    
+                  console.log('done');
+              } else {
+                  console.log('error');
+                  i--;
+              }             
+  }
+},
+    detectPersonalLevel: function() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+},
+    ShowMyDB: function(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+},
+    toggleVisibleMyDB: function(){
+        if (personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
 
-    return objCopy;
+    writeYourGenres: function() {
+    for (let i = 1; i < 2; i++) {
+        // let genre  = prompt(`Ваш любимый жанр под номером ${i}`);  // ПЕРВЫЙ СПОСОБ
+        // if (genre === "" || genre == null ){
+        //     console.log('Вы ввели некорректные данные, или не ввели их вовсе');
+        //     i--;
+        // } else {
+        //     personalMovieDB.genres[i - 1] = genre;
+        // }
+        let genres  = prompt(`Введите выши любимые жанры через запятую`).toLowerCase;//ВТОРОЙ СПОСОБ
+        if (genres === "" || genres == null ){
+            console.log('Вы ввели некорректные данные, или не ввели их вовсе');
+            i--;
+        } else {
+            personalMovieDB.genres = genres.split(', ');
+            personalMovieDB.genres.sort();
+        }
+
+    }
+    personalMovieDB.genres.forEach((item, i) =>{
+        console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
 }
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
 };
 
-const newNumbers = copy (numbers); //КЛОНИРУЕМ ОБЪЕКТ
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
- //ЗАПОМНИТЬ. БЫВАЮТ ГЛУБОКИЕ И ПОВЕРХНОСТНЫЕ КОПИИ ОБЪЕКТОВ
-
-
- const add = {
-     d : 17,
-     e : 20
- };
-
- console.log(Object.assign(numbers, add)); //СОЕДИНЯЕМ 2 МАССИВА 
-
- const clone = Object.assign({}, add);
- clone.d = 20;
-
-//  console.log(add);
-//  console.log(clone);
-
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();  //КОПИРУЕМ МАССИВ
-
-newArray[1] = 'sadlkfjjksd';
-console.log(oldArray);
-console.log(newArray);
-
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook']; //СПРЕД МЕТОД РАЗВОРАЧИВАНИЯ И СКЛАДИРОВАНИЯ МАССИВОВ
-
-console.log(internet);
-hjbhjb
-
-
-
-function log (a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
 
 
 
 
-const array = ['a', 'b'];
-const newAaray = [...array];
-
-console.log(array);
-console.log(newAaray);
 
 
 
-const q = {
-    one: 1,
-    two: 2
-};
-const newObJ = {...q};
-console.log(newObJ);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
