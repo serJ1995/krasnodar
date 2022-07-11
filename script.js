d45766a39f98aeb33d29b85e590e22efc5d8f82e
Serj <webcoderprof@gmail.com>
2022-07-11 23:10:57 +0300

    tasks for the interview

--- script.js
+++ script.js
@@ -1,56 +1,72 @@
 'use strict';
 
-//To string
-// 1)]
- console.log(typeof(String(null)));
- console.log(typeof(String(4)));
+//  Какое будет выведено значение: 
+let x = 5; 
+alert( x++ ); 
+//ответ 5
 
- //2) конкатенация
- console.log(typeof(5 + ''));
-
-
-
- const num = 5;
- console.log("https://vk.com/catalog/" + num);
-
- const fontSize = 26 + 'px';
+//  Чему равно такое выражение: 
+[ ] + false - null + true
+console.log([] + false);//false
+console.log(typeof([] + false));//string
+console.log([] + false - null);//NaN
+console.log([] + false - null + true);//NaN
 
 
- //TO NUMBER
- //1)
- console.log(typeof(Number('4')));
- 
- //2)
-console.log(typeof(+ '5'));
-
-// 3)
-console.log(typeof(parseInt("15 px", 10)));
-
-let answ = +prompt("Hello", "");
-
-//TO BOOLEAN
-
-0, '', null, NaN, undefined; //FALSE
-// ВСЕ ОСТАЛЬНОЕ БУДЕТ true
-
-// 1)
-let switcher = null;
-if (switcher){
-    console.log("Working");
-}
-
-switcher = 1;
-if (switcher){
-    console.log("Working");
-}
 
 
-// 2)
-console.log(typeof(Boolean('4')));
+//  Что выведет этот код:
+ let y = 1;
+  let x = y = 2; 
+  alert(x); 
+  //ответ 2
 
-// 3)
-console.log(typeof(Boolean(!!"3536253")));
+//  Чему равна сумма
+ [ ] + 1 + 2
+ console.log([] + 1 + 2);
+ //12
 
+//  Что выведет этот код:
+ alert( '1'[0] );
+ //1
+
+//  Чему равно 
+console.log(2 && 1 && null && 0 && undefined);
+//null     & ЗАПИНАЕТСЯ НА ЛЖИ
+//         || ЗАПИНАЕТСЯ НА ПРАВДЕ
+
+//  Есть ли разница между выражениями?
+ console.log(!!( 1 && 2 ) === (1 && 2));
+ //false 
+ // !! переводит в булиновое значение
+
+
+//  Что выведет этот код: 
+                  3
+alert( null || 2 && 3 || 4 ); 
+// 3
+
+
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+console.log(a == b);
+//  Правда ли что a == b ?
+// false ДВА РАЗНЫХ ХРАНИЛИЩА С ОДИНАКОВЫМИ ДАННЫМИ
+
+
+//  Что выведет этот код:
+ alert( +"Infinity" ); 
+ // Infinity
+
+//  Верно ли сравнение:
+"Ёжик" > "яблоко";
+console.log("Ёжик" > "яблоко");
+//false НУЖНО СМОТРЕТЬ ЮНИКОД
+
+//  Чему равно
+console.log(0 || "" || 2 || undefined || true || falsе);
+//2 
+// || ЗАПИНАЕТСЯ НА ПРАВДЕ? И ДАЛЬШЕ КОД НЕ ПОЙДЕТ
 
 
 
