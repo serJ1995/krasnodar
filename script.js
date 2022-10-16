832acd51bead1e7968a4d30eb9dedb8702817d31
Serj <webcoderprof@gmail.com>
2022-10-16 12:50:04 +0300

    getElementByPage

--- script.js
+++ script.js
@@ -1,73 +1,22 @@
 'use strict';
 
-//  Какое будет выведено значение: 
-let x = 5; 
-alert( x++ ); 
-//ответ 5
-
-//  Чему равно такое выражение: 
-[ ] + false - null + true
-console.log([] + false);//false
-console.log(typeof([] + false));//string
-console.log([] + false - null);//NaN
-console.log([] + false - null + true);//NaN
+const box = document.getElementById('box');
+console.log(box);
 
 
+const btns = document.getElementsByTagName('button');//ПОЛУЧАЕМ ТОЛЬКО ВТОРУЮ КНОПКУ  [1] после button
+console.log(btns[1]);//ВТОРОЙ ВАРИАНТ ТАКОГО ЖЕ ДЕЙСТВИЯ^
 
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-//  Что выведет этот код:
- let y = 1;
-  let x = y = 2; 
-  alert(x); 
-  //ответ 2
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach(item=>{
+  console.log(item);
+});//ПОЛУЧАЕМ ЭЛЕМЕНТЫ С УКАЗАННЫМ КЛАССОМ
 
-//  Чему равна сумма
- [ ] + 1 + 2
- console.log([] + 1 + 2);
- //12
-
-//  Что выведет этот код:
- alert( '1'[0] );
- //1
-
-//  Чему равно 
-console.log(2 && 1 && null && 0 && undefined);
-//null     & ЗАПИНАЕТСЯ НА ЛЖИ
-//         || ЗАПИНАЕТСЯ НА ПРАВДЕ
-
-//  Есть ли разница между выражениями?
- console.log(!!( 1 && 2 ) === (1 && 2));
- //false 
- // !! переводит в булиновое значение
-
-
-//  Что выведет этот код: 
-                  3
-alert( null || 2 && 3 || 4 ); 
-// 3
-
-
-const a = [1, 2, 3];
-const b = [1, 2, 3];
-console.log(a == b);
-//  Правда ли что a == b ?
-// false ДВА РАЗНЫХ ХРАНИЛИЩА С ОДИНАКОВЫМИ ДАННЫМИ
-
-
-//  Что выведет этот код:
- alert( +"Infinity" ); 
- // Infinity
-
-//  Верно ли сравнение:
-"Ёжик" > "яблоко";
-console.log("Ёжик" > "яблоко");
-//false НУЖНО СМОТРЕТЬ ЮНИКОД
-
-//  Чему равно
-console.log(0 || "" || 2 || undefined || true || falsе);
-//2 
-// || ЗАПИНАЕТСЯ НА ПРАВДЕ? И ДАЛЬШЕ КОД НЕ ПОЙДЕТ
-
+const oneHeart = document.querySelector('.heart');// СЕЛЕКТОР ПОЗВОЛЯЕТ ПОЛУЧИТЬ ПЕРВЫЙ ЭЛЕМЕНТ НА СТРАНИЦЕ ПО ДЕРЕВУ
+console.log(oneHeart);
 
 
 
